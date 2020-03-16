18720fddba1f9bfbb373a52e4bfcf7e43f30bef2
Erik Schierboom <erik_schierboom@hotmail.com>
2020-03-16 10:12:36 +0100

    Normalize whitespace when comparing representations
    Fixes cross-platform issues

--- test/Exercism.Representers.CSharp.IntegrationTests/TestSolutionRepresentationReader.cs
+++ test/Exercism.Representers.CSharp.IntegrationTests/TestSolutionRepresentationReader.cs
@@ -8,12 +8,15 @@ namespace Exercism.Representers.CSharp.IntegrationTests
             new TestSolutionRepresentation(solution.ReadExpected(), solution.ReadActual());
 
         private static string ReadActual(this TestSolution solution) =>
-            solution.ReadFile("representation.txt");
+            solution.ReadFile("representation.txt").NormalizeWhiteSpace();
 
         private static string ReadExpected(this TestSolution solution) =>
-            solution.ReadFile("expected_representation.txt");
+            solution.ReadFile("expected_representation.txt").NormalizeWhiteSpace();
 
         private static string ReadFile(this TestSolution solution, string fileName) =>
             File.ReadAllText(Path.Combine(solution.Directory, fileName));
+
+        private static string NormalizeWhiteSpace(this string str) =>
+            str.Replace("\r\n", "\n").Trim();
     }
 }
