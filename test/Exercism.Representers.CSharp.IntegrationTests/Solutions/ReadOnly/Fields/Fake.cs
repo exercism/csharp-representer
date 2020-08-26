27fd4c456c8059a052083e7b5cce727780b09e7d
mikedamay <mikemay.mdamay@googlemail.com>
2020-08-26 09:18:32 -0400

    readonly - fixed review points

--- test/Exercism.Representers.CSharp.IntegrationTests/Solutions/ReadOnly/Fields/Fake.cs
+++ test/Exercism.Representers.CSharp.IntegrationTests/Solutions/ReadOnly/Fields/Fake.cs
@@ -2,6 +2,8 @@ using System;
 
 public class Fake
 {
+    private static readonly string sroField = "interesting";
+    
     private readonly int roField = 1729; 
     
     public static string Test()
