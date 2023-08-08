68a3ac37a934149fa9d3ecd5fd46ab6a26126853
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-08 11:30:30 +0200

    Remove bulk updater

--- src/Exercism.Representers.CSharp.Bulk/BulkSolutionRepresentation.cs
+++
@@ -1,7 +0,0 @@
-namespace Exercism.Representers.CSharp.Bulk;
-
-internal class BulkSolutionRepresentation
-{
-    public string Representation { get; }
-    public BulkSolutionRepresentation(string representation) => Representation = representation;
-}
