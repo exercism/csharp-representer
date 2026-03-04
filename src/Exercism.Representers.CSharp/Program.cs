26d3f42eefa573039ed627f5fb59d9cb1e1de8e1
Erik Schierboom <erik_schierboom@hotmail.com>
2026-03-04 14:24:06 +0100

    Verwijder ongebruikte klasse

--- src/Exercism.Representers.CSharp/Program.cs
+++ src/Exercism.Representers.CSharp/Program.cs
@@ -25,8 +25,3 @@ public static class Program
         Console.WriteLine($"Created representation for {options.Slug} solution in directory {options.OutputDirectory}");
     }
 }
-
-internal static class StringExtensions
-{
-    internal static string Normalized(this string str) => str.TrimEnd().Replace("\r\n", "\n") + "\n";
-}
